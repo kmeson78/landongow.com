fc18b5efd3422f69ade555be99bd9f00b6138916
kmeson78 <landon.gow@gmail.com>
2026-09-04 06:00:18 -0600

    Delete pyratedial-config.js

--- pyratedial-config.js
+++
@@ -1,7 +0,0 @@
-// Pyrate Dial live station configuration.
-// The API key stays in this one file so deployment setup is a single edit.
-window.PYRATE_DIAL_CONFIG = {
-  youtubeApiKey: "",
-  youtubeHandle: "@landon78",
-  refreshMs: 120000
-};
